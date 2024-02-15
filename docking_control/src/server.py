a07514feddc17f6e994bae6eb6b371cf1e9d791e
Malik <malikibnueasa@gmail.com>
2024-02-15 21:50:18 +0530

    test service

---
+++ docking_control/src/server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import rospy
+from docking_control.srv import DockingControll
+
+def callback(msg):
+    response = (msg.a+msg.b)
+     
+    return response
+    
+if __name__ == '__main__':
+    rospy.init_node('docking_control')
+    s = rospy.Service('docking_service', DockingControll, callback)
+    rospy.loginfo("Docking_controll_live")
+    rospy.spin()
