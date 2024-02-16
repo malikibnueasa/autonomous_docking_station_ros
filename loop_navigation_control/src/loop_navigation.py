767d9a8013807c8d6d3a1b2d5f9a48b7e4be0173
Malik <malikibnueasa@gmail.com>
2024-02-16 10:08:28 +0530

    navigarion loop

--- loop_navigation_control/src/loop_navigation.py
+++ loop_navigation_control/src/loop_navigation.py
@@ -26,7 +26,7 @@ class MoveNode:
         wait = self.client.wait_for_result()
 
         if wait:
-            sleep(2)
+            sleep(1)
 
         if not wait:
             rospy.logerr("Action server not available!")
@@ -40,12 +40,15 @@ class MoveNode:
             self.go_to(0.3327469671548591, -0.07669310109807612, -0.34303521623827754)
         elif loc == 1:
             rospy.loginfo("Heading to Second Point")
-            self.go_to(0.0745602489172651, -2.7893164804901556, -2.65709415824142)
+            self.go_to(1.8487239882015116, 1.9633121738718773, -0.6458175417427996)
         elif loc == 2:
             rospy.loginfo("Heading to Third Point")
             self.go_to(2.57994547476032, -1.925609675033026, -1.5745951272044802)
         elif loc == 3:
             rospy.loginfo("Heading to Fourth Point")
+            self.go_to(4.3941970305451905, -0.23586030441848677, -0.10364319482621749)
+        elif loc == 4:
+            rospy.loginfo("Heading to Charging Docking Station")
             self.go_to(4.3941970305451905, -0.23586030441848677, -0.10364319482621749)
 
     def cancel_goal(self):
@@ -53,8 +56,10 @@ class MoveNode:
         rospy.loginfo("Goal cancelled")
     
     def docking_station_navigation(self):
+        
         self.cancel_goal()
-        pass
+        self.home_position(4)
+        
 
     def loop_navigation(self):
         if self.counter == 0:
@@ -74,16 +79,31 @@ class MoveNode:
         self.is_charging_flag = msg.data
 
     def run(self):
-        rospy.init_node('navigation_logical_node')
-        rospy.Subscriber('battery_low_power_status', Bool, self.flag_check)
-        while not rospy.is_shutdown():
-            while not self.is_charging_flag:
-                self.loop_navigation()
-            while self.is_charging_flag:
-                self.docking_station_navigation()
+        try:
+            rospy.init_node('navigation_logical_node')
+            rospy.Subscriber('battery_low_power_status', Bool, self.flag_check)
+            while not rospy.is_shutdown():
+                while not self.is_charging_flag:
+                    self.loop_navigation()
+                while self.is_charging_flag:
+                    self.docking_station_navigation()
+        except rospy.ROSInterruptException:
+            # Exception thrown when rospy.is_shutdown() is True
+            self.cancel_goal()
+            rospy.loginfo("Shutting down...")
+        except KeyboardInterrupt:
+            # Exception thrown when the user presses Ctrl+C
+            self.cancel_goal()
+            rospy.loginfo("Keyboard interrupt received. Shutting down...")
+        except Exception as e:
+            # Catch any other exceptions and handle them
+            self.cancel_goal()
+            rospy.logerr("An error occurred: %s", str(e))
+
 
 
 if __name__ == '__main__':
     rospy.init_node('navigation_logical_node')  
     move_node = MoveNode()
     move_node.run()
+
